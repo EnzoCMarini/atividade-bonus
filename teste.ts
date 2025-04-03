a2d8ebddbb8d512f0c8cee2c8cfa8ef8be999b8a
EnzoCMarini <enzocassao@gmail.com>
2025-04-03 15:18:01 -0300

    teste

---
+++ teste.ts
@@ -0,0 +1,27 @@
+console.clear ()
+
+
+let teclado = require (`prompt-sync`)();
+
+let nome: string = `Enzo Cassão Marini`; //nome, frase,
+let nascimento: number = 2009;
+let atributo: string = `gordo`
+let condicao: boolean = false;
+
+console.log(`O meu nome é ${nome} nasci em ${nascimento} e sou ${atributo}.`);
+
+if (nascimento <= 2009){
+    console.log(`Sim, executei essa linha`);
+}
+else if (nascimento < 1900){
+    console.log(`Sim, executei essa linha`);
+}
+else{
+    console.log(`Sim, executei essa linha`);
+}
+
+nome = teclado (`Digite o seu nome: `);
+nascimento = parseInt(teclado(`Digite o ano de nascimento: `));
+atributo = teclado (`Digite o seu atributo: `);
+
+console.log(`Ola, meu nome é ${nome} nasci em ${nascimento} e sou ${atributo}.`);
